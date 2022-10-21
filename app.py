3c36233eb2669fd615667c77f1dab896de4b666f
rquilliet <remi.quilliet@gmail.com>
2022-10-21 10:39:45 +0200

    second commit

---
+++ app.py
@@ -0,0 +1,95 @@
+# mongo.py
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'jdg'
+app.config['MONGO_URI'] = 'mongodb://remi:asmasmasm@ds113936.mlab.com:13936/jdg' #Comment to connect to localhost:27017 db
+#TEST
+mongo = PyMongo(app,config_prefix='MONGO')
+APP_URL="http://localhost:5000"
+
+@app.route('/career/<player_name>', methods=['GET'])
+def get_career(player_name):
+	player = mongo.db.players
+	pl=player.find_one({'name':player_name})
+	if pl:
+		output={'name':pl['name'],'career':pl['career']}
+	else:
+		output="No player named "+str(player_name)
+	return jsonify({'result':output})
+
+@app.route('/players', methods=['GET'])
+def get_players():
+	player = mongo.db.players
+	teams = request.args.get('teams', None)
+	statement={}
+	conditions=[]
+	if teams!=None:
+		for team in teams.split(","):
+			condition={}
+			condition['career.clubs']={}
+			condition['career.clubs']['$regex']=".*"+team+".*"
+			conditions.append(condition)
+		
+	statement['$and']=conditions
+	
+	output=[]
+	for pl in player.find(statement):
+		output.append({'name':pl['name']})
+
+	return jsonify({'result':output})
+
+# @app.route('/test', methods=['GET'])
+# def get_test():
+# 	#player = mongo.db.players
+# 	teams = request.args
+# 	print(teams)
+# 	# statement={}
+# 	# conditions=[]
+# 	# if teams!=None:
+# 	# 	for team in teams.split(","):
+# 	# 		condition={}
+# 	# 		condition['career.clubs']={}
+# 	# 		condition['career.clubs']['$regex']=".*"+team+".*"
+# 	# 		conditions.append(condition)
+		
+# 	# statement['$and']=conditions
+	
+# 	# output=[]
+# 	# for pl in player.find(statement):
+# 	# 	output.append({'name':pl['name']})
+# 	#pl=player.find_one({'name':player_name})
+# 	#if pl:
+# 	#	output={'name' : pl['name'], 'career': pl['career']}
+# 	#else:
+# 	#	output="No player named " +str(player_name)
+# 	return jsonify({'result':teams})
+
+
+# @app.route('/star/', methods=['GET'])
+# def get_one_star(name):
+#   star = mongo.db.stars
+#   s = star.find_one({'name' : name})
+#   if s:
+#     output = {'name' : s['name'], 'distance' : s['distance']}
+#   else:
+#     output = "No such name"
+#   return jsonify({'result' : output})
+
+# @app.route('/star', methods=['POST'])
+# def add_star():
+#   star = mongo.db.stars
+#   name = request.json['name']
+#   distance = request.json['distance']
+#   star_id = star.insert({'name': name, 'distance': distance})
+#   new_star = star.find_one({'_id': star_id })
+#   output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+#   return jsonify({'result' : output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
